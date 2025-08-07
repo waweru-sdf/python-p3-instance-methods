2f9ff817be48395b2a0ffba0b1d0566fc8c3d876
waweru-sdf <felixwaweru200@gmail.com>
2025-08-07 16:26:55 +0300

    .

--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,10 @@ class Person:
 
     #Instance method definition
     pass
+    def talk(self):
+       print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+James = Person()
+James.talk()
+James.walk()    
